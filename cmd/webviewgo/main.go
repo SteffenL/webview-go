9978f773105fd9874cca607acc6d10ad8838934c
Steffen André Langnes <steffenl.sw@gmail.com>
2022-06-28 11:01:12 +0900

    Add webview to tool

--- cmd/webviewgo/main.go
+++ cmd/webviewgo/main.go
@@ -12,8 +12,71 @@ import (
 	"strings"
 )
 
+func fetch_webview(libs_dir string, set_env bool, version string) {
+	lib_dir := filepath.Join(libs_dir, "webview", version)
+	_, err := os.Stat(filepath.Join(lib_dir, "webview.h"))
+	if err != nil || version == "master" {
+		if err == nil {
+			err = os.RemoveAll(lib_dir)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		fmt.Printf("Fetching webview %s...\n", version)
+		os.MkdirAll(filepath.Dir(lib_dir), os.ModePerm) // TODO: reduce permissions
+		repo_owner := "SteffenL"
+		repo_name := "webview-nogo"
+		repo_slug := strings.Join([]string{repo_owner, repo_name}, "/")
+		repo_base_url := "https://github.com/" + repo_slug
+		archive_url := repo_base_url + "/archive"
+		url := fmt.Sprintf("%s/%s.tar.gz", archive_url, version)
+		curl_cmd := exec.Command("curl", "-sSL", url)
+		curl_stdout, err := curl_cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer curl_stdout.Close()
+		tar_cmd := exec.Command("tar", "-xf", "-", "-C", filepath.Dir(lib_dir))
+		tar_cmd.Stdin = curl_stdout
+		curl_cmd.Start()
+		_, err = tar_cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		rename_from_path := filepath.Join(filepath.Dir(lib_dir), strings.Join([]string{repo_name, version}, "-"))
+		rename_to_path := lib_dir
+		err = os.Rename(rename_from_path, rename_to_path)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if set_env {
+		get_env_cmd := exec.Command("go", "env", "-json")
+		env_output, err := get_env_cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		env := map[string]string{}
+		err = json.Unmarshal(env_output, &env)
+		if err != nil {
+			log.Fatal(err)
+		}
+		include_dir := lib_dir
+		if strings.Contains(env["CGO_CXXFLAGS"], include_dir) {
+			return
+		}
+		fmt.Printf("Updating the environment for webview %s...\n", version)
+		cxxflags := fmt.Sprintf("CGO_CXXFLAGS=%s \"-I%s\"", env["CGO_CXXFLAGS"], include_dir)
+		set_env_cmd := exec.Command("go", "env", "-w", cxxflags)
+		err = set_env_cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func fetch_mswebview2(libs_dir string, set_env bool, version string) {
-	lib_dir := filepath.Join(libs_dir, "webview2-" + version)
+	lib_dir := filepath.Join(libs_dir, "mswebview2", version)
 	_, err := os.Stat(filepath.Join(lib_dir, "Microsoft.Web.WebView2.nuspec"))
 	if err != nil || version == "latest" {
 		if err == nil {
@@ -71,11 +134,13 @@ func fetch_mswebview2(libs_dir string, set_env bool, version string) {
 func cmd_fetch_libs(cmd string, args []string) {
 	var libs_dir string
 	var set_env bool
-	var webviw2_version string
+	var webview_version string
+	var mswebview2_version string
 	flags := flag.NewFlagSet(cmd, flag.ExitOnError)
 	flags.StringVar(&libs_dir, "libs-dir", "libs", "Libraries output directory.")
 	flags.BoolVar(&set_env, "set-env", false, "Set environment variables for go.")
-	flags.StringVar(&webviw2_version, "webview2-version", "1.0.1150.38", "WebView2 version to use or \"latest\".")
+	flags.StringVar(&webview_version, "webview-version", "master", "webview version to use (branch, tag, commit).")
+	flags.StringVar(&mswebview2_version, "mswebview2-version", "1.0.1150.38", "Microsoft WebView2 version to use or \"latest\".")
 	flags.Parse(args)
 
 	if !filepath.IsAbs(libs_dir) {
@@ -86,8 +151,9 @@ func cmd_fetch_libs(cmd string, args []string) {
 		libs_dir = filepath.Join(wd, libs_dir)
 	}
 	os := runtime.GOOS
+	fetch_webview(libs_dir, set_env, webview_version)
 	if os == "windows" {
-		fetch_mswebview2(libs_dir, set_env, webviw2_version)
+		fetch_mswebview2(libs_dir, set_env, mswebview2_version)
 	}
 }
 
