3016998ecbd182826c95bcc414ff11e783c4c073
Steffen André Langnes <steffenl.sw@gmail.com>
2023-08-20 16:36:16 +0900

    Remove WebView2 from build script

--- script/build.bat
+++ script/build.bat
@@ -10,21 +10,6 @@ mkdir "%build_dir%"
 echo Webview directory: %src_dir%
 echo Build directory: %build_dir%
 
-:: If you update the nuget package, change its version here
-set nuget_version=1.0.1150.38
-echo Using Nuget Package microsoft.web.webview2.%nuget_version%
-if not exist "%script_dir%\microsoft.web.webview2.%nuget_version%" (
-	curl -sSLO https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-	nuget.exe install Microsoft.Web.Webview2 -Version %nuget_version% -OutputDirectory %script_dir%
-	echo Nuget package installed
-)
-
-if not exist "%build_dir%\WebView2Loader.dll" (
-	copy "%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\x64\WebView2Loader.dll" "%build_dir%"
-)
-
-
-
 echo Building Go examples
 mkdir build\examples\go
 set "CGO_CPPFLAGS=-I%script_dir%\microsoft.web.webview2.%nuget_version%\build\native\include"
